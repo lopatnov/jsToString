ec61d2af47125d77f1a83bca5a0920f9e6f56eb7
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2019-10-22 00:35:25 +0300

    Added number to string tests

--- test/javascripttostring.test.ts
+++ test/javascripttostring.test.ts
@@ -20,3 +20,47 @@ describe("Boolean to String tests", () => {
     expect(actual).toBe(expected);
   });
 });
+describe("Number to String tests", () => {
+  it("should convert 0", () => {
+    let actual = j2s(0);
+    let expected = "0";
+
+    expect(actual).toBe(expected);
+  });
+  it("should convert numbers 0 < N < 1", () => {
+    let actual = j2s(0.75);
+    let expected = "0.75";
+
+    expect(actual).toBe(expected);
+  });
+  it("should convert negative numbers", () => {
+    let actual = j2s(-123);
+    let expected = "-123";
+
+    expect(actual).toBe(expected);
+  });
+  it("should convert positive numbers", () => {
+    let actual = j2s(456);
+    let expected = "456";
+
+    expect(actual).toBe(expected);
+  });
+  it("should convert infinity", () => {
+    let actual1 = j2s(Number.POSITIVE_INFINITY);
+    let expected1 = "Number.POSITIVE_INFINITY";
+    let actual2 = j2s(Number.NEGATIVE_INFINITY);
+    let expected2 = "Number.NEGATIVE_INFINITY";
+
+    expect(actual1).toBe(expected1);
+    expect(actual2).toBe(expected2);
+  });
+  it("should convert min/max values", () => {
+    let actual1 = j2s(Number.MAX_VALUE);
+    let expected1 = "Number.MAX_VALUE";
+    let actual2 = j2s(Number.MIN_VALUE);
+    let expected2 = "Number.MIN_VALUE";
+
+    expect(actual1).toBe(expected1);
+    expect(actual2).toBe(expected2);
+  });
+});
